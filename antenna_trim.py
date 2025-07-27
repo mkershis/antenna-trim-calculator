8209bdfdf06f849b4dc5725f971613cf7371f7f6
Matt Kershis <mattkershis@gmail.com>
2025-07-27 11:07:27 -0400

    Add files via upload

---
+++ antenna_trim.py
@@ -0,0 +1,113 @@
+import streamlit as st 
+from math import floor
+
+def fractional_inches(decimal_inches: float, precision: str) -> str:
+    '''
+    Converts decimal inches to the closest fraction
+    with either a precision of 1/32" or 1/16"
+    '''
+    # map the user's choice to the correct starting point in the
+    # list of possible denominators
+
+    precision_to_index = {'1/32':0,'1/16':1}
+    index = precision_to_index[precision]
+
+    int_part = floor(decimal_inches)
+    frac_part = decimal_inches - int_part
+
+    # handle cases where decimal_inches is a whole number
+    if frac_part == 0:
+        return f'{int_part}'
+    else:
+        
+        denominators = [32, 16, 8, 4, 2]
+
+        # want to get to the closest fraction for accuracy
+        numerator = round(frac_part * denominators[index])
+
+        # to catch small fractions which are < 1/16 or < 1/32
+        if numerator == 0:
+            return f'{int_part}'
+        
+        # check if the numerator can be simplified, but don't let
+        # the loop overrun the list of denominators
+        while (numerator % 2 == 0) & (index < len(denominators) - 1):
+            numerator /= 2
+            index += 1
+        if int_part == 0:
+            # return f'{int(numerator)}/{denominators[index]}'
+            return r'\frac{{{}}}{{{}}}'.format(int(numerator), denominators[index])
+        elif numerator == denominators[index]:
+            return f'{int_part + 1}'
+        else:
+            # return f'{int_part} {int(numerator)}/{denominators[index]}'
+            return r'{}\frac{{{}}}{{{}}}'.format(int_part,int(numerator), denominators[index])
+
+def unit_convert(value: float, to_unit: str, precision: str=None) -> str:
+    '''
+    Takes an input length in feet and converts to
+    the desired unit. Returns the value along with a 
+    display unit for printing    
+    '''
+    if to_unit == 'feet/inches':
+        feet = floor(value)
+        decimal_inches = (value - feet) * 12
+        if feet > 0:
+            if decimal_inches == 0:
+                return f'$${feet}\\, ft$$'
+            else:
+                return f'$${feet}\\, ft\\quad{fractional_inches(decimal_inches, precision)}\\,in$$'
+        else:
+            if decimal_inches == 0:
+                return ''
+            else:
+                return f'$${fractional_inches(decimal_inches, precision)}\\, in$$'
+
+    elif to_unit == 'centimeters':
+        centimeters = value * 12 * 2.54
+        return f'$${centimeters:,.1f}\\, cm$$'
+
+intro_text = open('readme.md').read()
+
+st.markdown(intro_text)
+
+current_freq = st.number_input('Current resonant frequency (MHz)', format = '%0.3f', step = 1.0)
+desired_freq = st.number_input('Desired resonant frequency (MHz)', format = '%0.3f', step = 1.0)
+
+to_unit = st.radio(label = 'Select preferred display units', options = ['feet/inches','centimeters'], horizontal = True)
+if to_unit == 'feet/inches':
+    precision = st.radio(label = 'Select desired precision (inches)',options = ['1/32','1/16'], horizontal = True, index=1)
+else:
+    precision = None
+calculate = st.button('Run Calculation')
+
+if calculate & ((current_freq > 0) & (desired_freq > 0)):
+    st.divider()
+
+    diff_feet = abs((468 / current_freq) - (468 / desired_freq))
+    
+    full_trim = unit_convert(diff_feet, to_unit, precision)
+    half_trim = unit_convert(diff_feet / 2, to_unit, precision)
+
+    if current_freq == desired_freq:
+        st.write('Your antenna is perfectly tuned. No changes required.')
+    else:
+    
+        if current_freq < desired_freq:
+            st.write('Antenna is too long. Reduce total length by:')
+        elif current_freq > desired_freq:
+            st.write('Antenna is too short. Increase total length by:')
+
+        # st.metric(label = 'Full Trim', value = full_trim, border = True, label_visibility = 'collapsed')
+        st.markdown(full_trim)
+
+        if current_freq < desired_freq:
+            st.write('or trim each side of the dipole by:')
+        elif current_freq > desired_freq:
+            st.write('or extend each side of the dipole by:')
+
+        # st.metric(label = 'Half Trim', value = half_trim, border = True, label_visibility = 'collapsed')
+        st.markdown(half_trim)
+
+elif calculate & ((current_freq == 0) | (desired_freq == 0)):
+    st.markdown(':red[Error: Both frequencies must be greater than zero.]')
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Dipole Antenna Tuning Calculator
+
+This calculator uses the well-known formula:
+
+$$L = \frac{468}{f}$$
+
+
+for calculating the length (L, in feet) of a half-wave dipole antenna at a given frequency (f, in MHz). If you know what frequency you are aiming for, and have a measurement of where the antenna is currently resonant, you can use this equation to calculate the amount of wire you need to add or remove:
+
+$$\Delta L = \left|\frac{468}{f_{current}}-\frac{468}{f_{desired}}\right|$$
+
+Simply enter these two frequencies and hit "Run calculation". You can choose between imperial and metric units, and can also specify the precision for the imperial units to the closest $\frac{1}{32}''$ or $\frac{1}{16}''$ since you'll most likely be using a ruler or tape measure while tuning your antenna in the field.
